12b9e1d098529992b0fdfdb8c474e9be0e9ead6e
High Bridge <hainu738@gmail.com>
2025-11-30 22:59:53 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -2,3 +2,19 @@
 # SPDX-FileCopyrightText: 2025 highbrige-yayoi <hainu738@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
+# テスト用の入力ファイルを作成
+cat << EOF > test_input.txt
+1
+5
+2
+8
+3
+EOF
+
+# 期待される出力
+# Maxima: 5, 8
+# Minima: 2
+expected_output="5
+8
+
+2"
